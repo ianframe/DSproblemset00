9ff4ca61c487d86a17065cfff2c7096182659030
ianframe <ian.frame@hies.org>
2017-06-16 09:16:49 -0400

    starter code upload

---
+++ ProblemSet00.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.lang.Exception;
+
+/*
+*	Note that because you are working with different text files for each problem,
+*	you will need to create separate readers for each individual problem. 
+*/	
+public class ProblemSet00
+{
+	public static void helloWorld()
+	{}
+
+	public static void pokemon()
+	{}
+
+	public static void triangularNumbers()
+	{}
+
+	public static void marioPyramid()
+	{}
+}
